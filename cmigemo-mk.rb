a81c74ce04c8c4aaade82049542c12d3019b08cd
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2016-04-29 21:01:32 +0900

    update cmigemo and travis test

--- cmigemo-mk.rb
+++ cmigemo-mk.rb
@@ -6,21 +6,18 @@ class CmigemoMk < Formula
 
   depends_on 'nkf' => :build
 
-  option 'with-binary-release', ''
-
   def patches
     DATA
   end
 
   def install
-    ENV["HOMEBREW_OPTFLAGS"] = "-march=core2" if build.with? 'binary-release'
     ENV.append 'LDFLAGS', '-headerpad_max_install_names'
+
     system "./configure", "--prefix=#{prefix}"
     system "make osx-dict"
     cd 'dict' do
       system "make utf-8"
     end
-
     ENV.j1 # Install can fail on multi-core machines unless serialized
     system "make osx-install"
   end
