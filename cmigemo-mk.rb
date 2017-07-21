ae207b81c7a2900100ea7544269969f6b7b15cf1
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2017-07-21 23:49:09 +0900

    v0.1 release

--- cmigemo-mk.rb
+++ cmigemo-mk.rb
@@ -1,20 +1,19 @@
-require 'formula'
-
 class CmigemoMk < Formula
-  homepage 'http://www.kaoriya.net/software/cmigemo'
-  head 'https://github.com/koron/cmigemo', :using => :git
+  desc "migemo"
+  homepage "http://www.kaoriya.net/software/cmigemo"
+  head "https://github.com/koron/cmigemo", :using => :git
 
-  depends_on 'nkf' => :build
+  depends_on "nkf" => :build
 
   def install
-    ENV.append 'LDFLAGS', '-headerpad_max_install_names'
+    ENV.append "LDFLAGS", "-headerpad_max_install_names"
 
     system "./configure", "--prefix=#{prefix}"
-    system "make osx-dict"
-    cd 'dict' do
-      system "make utf-8"
+    system "make", "osx-dict"
+    cd "dict" do
+      system "make", "utf-8"
     end
     ENV.deparallelize
-    system "make osx-install"
+    system "make", "osx-install"
   end
 end
