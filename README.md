f3dfab6b0a9cfbfc891bf3d4b1ec616d9552b665
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2017-07-22 10:58:38 +0900

    Support 'brew audit --strict'

--- README.md
+++ README.md
@@ -1,27 +1,22 @@
 homebrew-macvim
 ===
 
-[![](https://img.shields.io/travis/KazuakiM/homebrew-macvim.svg)](https://travis-ci.org/KazuakiM/homebrew-macvim)
-[![](https://img.shields.io/github/issues/KazuakiM/homebrew-macvim.svg)](https://github.com/KazuakiM/homebrew-macvim/issues)
-[![](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+[![TravisCI](https://travis-ci.org/KazuakiM/homebrew-macvim.svg?branch=master)](https://travis-ci.org/KazuakiM/homebrew-macvim)
+[![GitHub issues](https://img.shields.io/github/issues/KazuakiM/homebrew-macvim.svg?style=flat-square)](https://github.com/KazuakiM/homebrew-macvim/issues)
+[![license](https://img.shields.io/github/license/KazuakiM/homebrew-macvim.svg?style=flat-square)](https://raw.githubusercontent.com/KazuakiM/homebrew-macvim/master/LICENSE)
 
 This repository is inspired at [splhack](https://github.com/splhack/homebrew-splhack).
 
 ## Usage
 
-* Python 3.6
 ```bash
 $ brew install --HEAD kazuakim/macvim/macvim-kaoriya
 ```
-* Python 3.5(stable)
-```bash
-$ brew install kazuakim/macvim/macvim-kaoriya
-```
 
-##Author
+## Author
 
 [KazuakiM](https://github.com/KazuakiM/)
 
-##License
+## License
 
 This software is released under the MIT License, see LICENSE.
--- macvim-kaoriya.rb
+++ macvim-kaoriya.rb
@@ -1,20 +1,18 @@
 class MacvimKaoriya < Formula
   desc "GUI for vim, made for OS X"
   homepage "https://github.com/splhack/macvim-kaoriya"
+  url "https://github.com/splhack/macvim/archive/20151225.tar.gz"
+  #sha1 "40da9b5f8ff5b757e3597fdbd0bab0f5c8c06ee4"
+  revision 1
   head "https://github.com/splhack/macvim.git"
 
-
-  depends_on "cmigemo-mk" => :build
   depends_on "universal-ctags/universal-ctags/universal-ctags" => :build
+  depends_on "cmigemo" => :build
   depends_on "gettext" => :build
   depends_on "perl"    => :build
   depends_on "python3" => :build
-  depends_on "ruby"    => :build
   depends_on "lua"     => :build
   depends_on "luajit"  => :build
-
-  PYTHON3_CONFIG = `python3-config --prefix|tr -d '\n'`
-  RUBY_WHICH     = `which ruby|tr -d '\n'`
 
   def install
     ENV.clang if MacOS.version >= :lion
@@ -29,13 +27,12 @@ class MacvimKaoriya < Formula
       "--enable-fail-if-missing",
       "--enable-luainterp",
       "--enable-multibyte",
-      "--enable-rubyinterp",
+      "--enable-rubyinterp=dynamic",
       "--enable-perlinterp",
       "--enable-python3interp",
       "--prefix=#{prefix}",
       "--with-features=huge",
-      "--with-ruby-command=#{RUBY_WHICH}",
-      "--with-python3-config-dir=#{PYTHON3_CONFIG}/lib/python3.6/config-3.6m-darwin",
+      "--with-python3-config-dir=#{HOMEBREW_PREFIX}/Frameworks/Python.framework/Versions/3.6/lib/python3.6/config-3.6m-darwin",
       "--with-luajit",
       "--with-lua-prefix=#{HOMEBREW_PREFIX}",
       "--with-tlib=ncurses"
@@ -61,5 +58,9 @@ class MacvimKaoriya < Formula
     Dir.glob("#{HOMEBREW_PREFIX}/share/migemo/utf-8/*").each do |f|
       cp f, dict
     end
+  end
+
+  test do
+    system bin/"vim", "--version"
   end
 end
